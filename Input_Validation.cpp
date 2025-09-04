081495fdf9290235b7eb5f2b838a9867801cdcea
Mohamed-Owaymr <clantrs3@gmail.com>
2025-09-04 10:21:26 +0300

    Initilize Commit

---
+++ Input_Validation.cpp
@@ -0,0 +1,198 @@
+#include "Input_Validation.h"
+
+
+bool Input_Validation::isNegative(float value)
+{
+    return value < 0;
+}
+bool Input_Validation::isPositive(float value)
+{
+    return value > 0;
+}
+bool Input_Validation::isZero(float value)
+{
+    return value == 0;
+}
+void Input_Validation::cleanCin()
+{
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+void Input_Validation::printInvalidValueErrorMessage()
+{
+    cout << "Invalid input, please enter a valid number";
+}
+float Input_Validation::readPositiveNumber(const string& msg)
+{
+    float number;
+    cout << msg << ": ";
+    cin >> number;
+    while(cin.fail() || isNegative(number) || isZero(number))
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        cout << msg << " Again: ";
+        cin >> number;
+    }
+    return number;
+}
+float Input_Validation::readNegativeNumber(const string& msg)
+{
+    float number;
+    cout << msg << ": ";
+    cin >> number;
+    while(cin.fail() || isPositive(number) || isZero(number))
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        cout << msg << " Again: ";
+        cin >> number;
+    }
+    return number;
+}
+float Input_Validation::readRationalNumber(const string& msg)
+{
+    float number;
+    cout << msg << ": ";
+    cin >> number;
+    while(cin.fail())
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        cout << msg << " Again: ";
+        cin >> number;
+    }
+    return number;
+}
+char Input_Validation::readLetter(const string& msg)
+{
+    char input;
+    cout << msg <<": ";
+    cin >> input;
+    while(cin.fail() || !isalpha(input))
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        cout << msg << " Again: ";
+        cin >> input;
+    }
+    return input;
+}
+bool Input_Validation::isFalseLetterCharacter(char letter)
+{
+    return letter == 'n' || letter == 'N';
+}
+bool Input_Validation::isTrueLetterCharacter(char letter)
+{
+    return letter == 'y' || letter == 'Y';
+}
+bool Input_Validation::readYesNoAsBool(const string& msg)
+{
+    char letter = readLetter(msg);
+    while(!(isFalseLetterCharacter(letter) || isTrueLetterCharacter(letter)))
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        letter = readLetter(msg);
+    }
+    if(isFalseLetterCharacter(letter))
+        return false;
+    else
+        return true;
+}
+bool Input_Validation::isInRange(float from , float to , float number , bool inclusive)
+{
+    if(inclusive)
+        return number >= from && number <= to;
+    else
+        return number > from && number < to;
+}
+float Input_Validation::readNumberInRange(const string& msg , float from , float to , bool inclusive)
+{
+    float number;
+    cout << msg << ": ";
+    cin >> number;
+    while(cin.fail() || !isInRange(from , to , number , inclusive))
+    {
+        cleanCin();
+        printInvalidValueErrorMessage();
+        cout << msg << " Again: ";
+        cin >> number;
+    }
+    return number;
+}
+void Input_Validation::displayUserOptionsList(const string& options)
+{
+    cout << "\n****************************\n";
+    cout << "Your Options: \n";
+    cout << options << endl;
+    cout << "****************************\n";
+}
+float Input_Validation::readNumber(const string& msg , enNumberTypes inputType)
+{
+    float inputNumber = 0;
+    switch (inputType)
+    {
+    case enNumberTypes::NaturalNum:
+        inputNumber = readRationalNumber(msg);
+        break;
+    case enNumberTypes::PositiveNum:
+        inputNumber = readPositiveNumber(msg);
+        break;
+    case enNumberTypes::NegativeNum:
+        inputNumber = readNegativeNumber(msg);
+        break;
+
+    
+    default:
+        inputNumber = readPositiveNumber(msg);
+        break;
+    }
+    return inputNumber;
+}
+template <typename EnumType>
+size_t Input_Validation::getEnumSizeUsingMagicEnumCount()
+{
+    static_assert(is_enum<EnumType>::value, "Error: The parameter must be an enum!");
+    return magic_enum::enum_count<EnumType>();
+}
+template<typename EnumType>
+bool Input_Validation::isValidOptionNumber(short opNumber)
+{
+    size_t enumSize = getEnumSizeUsingMagicEnumCount<EnumType>();
+    return opNumber <= enumSize && !isNegative(opNumber);
+}
+template<typename EnumType>
+EnumType Input_Validation::readOptionNumber(const string& msg, const string& optionsList)
+{
+    short optionNumber;
+    displayUserOptionsList(optionsList);
+    optionNumber = readPositiveNumber(msg);
+    while (!isValidOptionNumber<EnumType>(optionNumber))
+    {
+        printInvalidValueErrorMessage();
+        optionNumber = readPositiveNumber(msg);
+    }
+    return static_cast<EnumType>(optionNumber);
+}
+string Input_Validation::readString(const string& msg , short minLength , short maxLength , bool inclusive)
+{
+    if(!isCinBufferEmpty())
+        cleanCin();
+
+    string input;
+    cout << msg << ": ";
+    getline(cin , input);
+    short length = input.length();
+    while(input.empty() || !isInRange(minLength , maxLength , length , inclusive))
+    {
+        cout << msg << " Again: ";
+        getline(cin , input);
+        length = input.length();
+    }
+    return input;
+}
+bool Input_Validation::isCinBufferEmpty()
+{
+    return cin.rdbuf()->in_avail() == 0;
+}
---
+++ Input_Validation.h
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <limits>
+#include <cctype>
+#include <string>
+#include "magic_enum.hpp"
+using namespace std;
+
+class Input_Validation
+{
+private:
+    static void printInvalidValueErrorMessage();
+    static void displayUserOptionsList(const string& options);
+
+    template <typename EnumType>
+    static size_t getEnumSizeUsingMagicEnumCount();
+
+    static bool isCinBufferEmpty();
+
+public:
+    static bool   isNegative(float value);
+    static bool   isPositive(float value);
+    static bool   isZero(float value);
+    static void   cleanCin();
+    static float  readPositiveNumber(const string& msg);
+    static float  readNegativeNumber(const string& msg);
+    static float  readRationalNumber(const string& msg);
+    static char   readLetter(const string& msg);
+    static bool   isFalseLetterCharacter(char letter);
+    static bool   isTrueLetterCharacter(char letter);
+    static bool   readYesNoAsBool(const string& msg);
+    static bool   isInRange(float from , float to , float number , bool inclusive  = true);
+    static float  readNumberInRange(const string& msg , float from , float to , bool inclusive  = true);
+
+
+    template <typename EnumType>
+    static size_t getEnumSizeUsingMagicEnumCount();
+
+    template<typename EnumType>
+    static bool isValidOptionNumber(short opNumber);
+
+    template<typename EnumType>
+    static EnumType readOptionNumber(const string& msg, const string& optionsList);
+
+    enum enNumberTypes{NaturalNum = 1 , PositiveNum = 2 , NegativeNum = 3};
+
+    static float readNumber(const string& msg , enNumberTypes inputType);
+
+    static string readString(const string& msg , short minLength = 0 , short maxLength = 1000 , bool inclusive = true);
+};
